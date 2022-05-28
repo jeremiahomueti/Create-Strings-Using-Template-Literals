1fe9a3ca9703374c441a4d6ce81433eda1502f99
jeremiahomueti <jeremiahomueti@gmail.com>
2022-05-28 13:36:14 +0100

    Update

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 //Create strings using template literals!!
 
+const person =
+{
+    name: "Zodiac Hasbro", 
+    age: 56
+};
+
+const greeting = `Hello, my name is ${person.name}!  
+I am ${person.age} years old.`; // here's an example of a template literal (using back-tick. So my guess is that template literals often require the use of back-ticks.)
+
+console.log(greeting);
+
